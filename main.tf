d4b333e263681edccac0e4dbc22549949208b668
Adetunji Adeshina <59351722+tunjiadeshina@users.noreply.github.com>
2021-09-07 18:54:38 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,6 @@ resource "aws_instance" "testInstance" {
   instance_type = "${var.instance_type}"
   subnet_id = "${aws_subnet.subnet_public.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  key_name = "${aws_key_pair.ec2key.key_name}"
  tags = {
   "Environment" = "${var.environment_tag}"
  }
